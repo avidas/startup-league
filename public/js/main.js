736dde2ab4b8990c6969ee37f87c5294a736dc12
Avi Das <andas222@gmail.com>
2015-05-10 12:47:05 -0500

    update flash and home page

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
 
   // Place JavaScript code here...
+  // 
+  // 
+    function animateRows() {
+        // simple animation to fade in all but the top story
+        
+        var rowNum = 0;
+        var dur = 50;
+        $(".feed li").each(function() {
+            console.log("here");
+            if (rowNum === 1) { 
+                // skip 1st row
+                return; 
+            } 
+            // capture current row
+            var elm = $(this);
+            console.log(elm);
+            // schedule it to fade in
+            setTimeout(function() { 
+                console.log("here");
+                elm.fadeIn();
+            }, dur); 
+            dur += 50;
+            rowNum += 1;
+        });
+    };
 
+    function disFlash() {
+      setTimeout(function(){
+        $('.alert').slideUp(500);
+      }, 1000);
+    };
+
+    //animateRows();
+    disFlash();
 });
